1941347716d90b19b4825e97c1f8881e55a5284d
Benadette Mokandu <benadettemokandu@gmail.com>
2025-08-21 11:27:18 +0300

    initial commit

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 class Person:
-    # Class body goes here
+    def talk (self):
+        print ("Hello World!")
+    def walk (walk):
+        print ("The person is walking.")
 
-    #Instance method definition
-    pass
+Benadette = Person ()
+Benadette.talk()
+Benadette.walk()
